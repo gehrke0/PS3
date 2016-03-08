c3679b05f15d342dc55346693e6c44deae3f9263
Carter Gehrke <gehrke@udel.edu>
2016-03-08 11:45:18 -0500

    initial commit

---
+++ PS3/src/main/java/base/Account.java
@@ -0,0 +1,10 @@
+package base;
+
+import java.util.Date;
+
+public class Account {
+	private int id = 0;
+	private double balance = 0.0;
+	private double annualInterestRate = 0.0;
+	private Date dateCreated;
+}
