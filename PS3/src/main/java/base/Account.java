097badd28779db8b30be9e5b0252f14998d40e76
Carter Gehrke <gehrke@udel.edu>
2016-03-09 14:20:13 -0500

    done class

--- PS3/src/main/java/base/Account.java
+++ PS3/src/main/java/base/Account.java
@@ -2,9 +2,48 @@ package base;
 
 import java.util.Date;
 
-public class Account {
+public class Account{
 	private int id = 0;
-	private double balance = 0.0;
-	private double annualInterestRate = 0.0;
+	private double balance = 0;
+	private double annualInterestRate = 0;
 	private Date dateCreated;
+	private int getId() {
+		return id;
+	}
+	private void setId(int id) {
+		this.id = id;
+	}
+	private double getBalance() {
+		return balance;
+	}
+	private void setBalance(double balance) {
+		this.balance = balance;
+	}
+	private double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	private void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+	private Date getDateCreated() {
+		return dateCreated;
+	}
+	private void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public Account(){}
+	public Account(int id, double balance){
+		this.id = id;
+		this.balance = balance;
+	}
+	public double getMonthlyInterestRate(){
+		return annualInterestRate/12;
+	}
+	public double withdraw(double moneyWithdrawn){
+		
+		return balance - moneyWithdrawn;
+	}
+	public double deposit(double moneyDeposited){
+		return balance + moneyDeposited;
+	}
 }
